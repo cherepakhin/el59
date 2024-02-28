651c2472a180f282fc652e964bdaa236b5ddc9c8
Черепахин Василий <vasi.che@gmail.com>
2024-02-28 13:35:33 +0500

    fix ShopRightProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/ShopRightProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/ShopRightProvider.java
@@ -1,13 +1,66 @@
 package ru.perm.v.el59.office.dao.impl;
 
+import ru.el59.office.shopmodel.ShopRight;
 import ru.perm.v.el59.office.iproviders.shopmodel.IShopRightProvider;
-import ru.perm.v.el59.office.shopmodel.ShopRight;
 import ru.perm.v.el59.office.wscommand.impl.GenericDaoMessageImpl;
 
+import java.util.List;
+
+//TODO: fill methods
 public class ShopRightProvider extends GenericDaoMessageImpl<ShopRight, Long>
 		implements IShopRightProvider {
 
 	public ShopRightProvider(Class<ShopRight> type) {
 		super(type);
+	}
+
+	@Override
+	public Long create(ShopRight shopRight) throws Exception {
+		return null;
+	}
+
+	@Override
+	public ShopRight read(Long aLong) {
+		return null;
+	}
+
+	@Override
+	public void update(ShopRight shopRight) throws Exception {
+
+	}
+
+	@Override
+	public void delete(ShopRight shopRight) throws Exception {
+
+	}
+
+	@Override
+	public List<ShopRight> getByCritery(Object o) {
+		return null;
+	}
+
+	@Override
+	public ShopRight initialize(Long aLong) {
+		return null;
+	}
+
+	@Override
+	public ShopRight getByEqName(String s) {
+		return null;
+	}
+
+	@Override
+	public List<ShopRight> getByLikeName(String s) {
+		return null;
+	}
+
+	@Override
+	public Class<ShopRight> getType() {
+		return null;
+	}
+
+	@Override
+	public Long getMax() {
+		return null;
 	}
 }
