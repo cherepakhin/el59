f39415ec695c8c7cddc199a0e1e9ff960d2b53e4
Черепахин Василий <vasi.che@gmail.com>
2024-02-28 12:57:17 +0500

    fix OperationProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/OperationProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/OperationProvider.java
@@ -1,47 +1,47 @@
 package ru.perm.v.el59.office.dao.impl;
 
+import ru.el59.office.db.Operation;
+import ru.perm.v.el59.office.iproviders.IOperationProvider;
+import ru.perm.v.el59.office.iproviders.dao.CommonCritery;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import ru.perm.v.el59.dao.CommonCritery;
-import ru.perm.v.el59.office.db.Operation;
-import ru.perm.v.el59.office.iproviders.IOperationProvider;
-
 public class OperationProvider extends GenericDaoHibernateImpl<Operation, Long>
-		implements IOperationProvider {
+        implements IOperationProvider {
 
-	private List<Operation> listAll ;
-	
-	public OperationProvider(Class<Operation> type) {
-		super(type);
-	}
+    private List<Operation> listAll;
 
-	public List<Operation> getAll() {
-		if(listAll==null) {
-			CommonCritery critery = new CommonCritery("");
-			listAll=getByCritery(critery);
-		}
-		return listAll;
-	}
+    public OperationProvider(Class<Operation> type) {
+        super(type);
+    }
 
-	@Override
-	public Map<String, Operation> getAllAsHashBestChr() {
-		List<Operation> operations = getAll();
-		HashMap<String, Operation> ret = new HashMap<String, Operation>();
-		for (Operation operation : operations) {
-			ret.put(operation.getBest(), operation);
-		}
-		return ret;
-	}
+    public List<Operation> getAll() {
+        if (listAll == null) {
+            CommonCritery critery = new CommonCritery("");
+            listAll = getByCritery(critery);
+        }
+        return listAll;
+    }
 
-	@Override
-	public Map<String, Operation> getHashChrOperation() {
-		List<Operation> operations = getAll();
-		HashMap<String, Operation> ret = new HashMap<String, Operation>();
-		for (Operation operation : operations) {
-			ret.put(operation.getChr(), operation);
-		}
-		return ret;
-	}
+    @Override
+    public Map<String, Operation> getAllAsHashBestChr() {
+        List<Operation> operations = getAll();
+        HashMap<String, Operation> ret = new HashMap<String, Operation>();
+        for (Operation operation : operations) {
+            ret.put(operation.getBest(), operation);
+        }
+        return ret;
+    }
+
+    @Override
+    public Map<String, Operation> getHashChrOperation() {
+        List<Operation> operations = getAll();
+        HashMap<String, Operation> ret = new HashMap<String, Operation>();
+        for (Operation operation : operations) {
+            ret.put(operation.getChr(), operation);
+        }
+        return ret;
+    }
 }
