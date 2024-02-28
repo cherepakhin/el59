0a8d553d79b3fe1e2590b3ba3149436662608e74
Черепахин Василий <vasi.che@gmail.com>
2024-02-28 10:21:03 +0500

    fix PayTerminalProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/shopmodel/PayTerminalProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/shopmodel/PayTerminalProvider.java
@@ -1,7 +1,8 @@
 package ru.perm.v.el59.office.dao.impl.shopmodel;
 
-import ru.perm.v.el59.office.iproviders.shopmodel.IPayTerminalProvider;
-import ru.perm.v.el59.office.shopmodel.PayTerminal;
+
+import ru.el59.office.iproviders.shopmodel.IPayTerminalProvider;
+import ru.el59.office.shopmodel.PayTerminal;
 
 public class PayTerminalProvider extends APaymentProvider<PayTerminal, Long>
 		implements IPayTerminalProvider {
