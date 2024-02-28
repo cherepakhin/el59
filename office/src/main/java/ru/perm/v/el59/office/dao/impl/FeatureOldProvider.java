99e9019c44cf3898a6f89d7f846ea364d3042788
Черепахин Василий <vasi.che@gmail.com>
2024-02-28 10:45:46 +0500

    fix FeatureOldProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/FeatureOldProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/FeatureOldProvider.java
@@ -1,7 +1,8 @@
 package ru.perm.v.el59.office.dao.impl;
 
-import ru.perm.v.el59.office.db.FeatureOld;
-import ru.perm.v.el59.office.iproviders.IFeatureOldProvider;
+
+import ru.el59.office.db.FeatureOld;
+import ru.el59.office.iproviders.IFeatureOldProvider;
 
 public class FeatureOldProvider extends
 		GenericDaoHibernateImpl<FeatureOld, Long> implements
