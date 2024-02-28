d3b89b0f4bff0708e5b64a20446d9912127abb15
Черепахин Василий <vasi.che@gmail.com>
2024-02-28 14:37:57 +0500

    fix TypeErrProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/TypeErrProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/TypeErrProvider.java
@@ -1,14 +1,12 @@
 package ru.perm.v.el59.office.dao.impl;
 
-import java.util.List;
+import ru.el59.office.db.TypeErr;
 
-import ru.perm.v.el59.office.db.TypeErr;
-import ru.perm.v.el59.office.iproviders.ITypeErrProvider;
 
 public class TypeErrProvider extends GenericDaoHibernateImpl<TypeErr, Long> {
 
-	public TypeErrProvider(Class<TypeErr> type) {
-		super(type);
-	}
+    public TypeErrProvider(Class<TypeErr> type) {
+        super(type);
+    }
 
 }
