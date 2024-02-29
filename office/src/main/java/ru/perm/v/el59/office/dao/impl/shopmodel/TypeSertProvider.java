d6b546abdd44c60b8c1fb35db1753949a21969df
Черепахин Василий <vasi.che@gmail.com>
2024-02-29 16:39:14 +0500

    fix TypeSertProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/shopmodel/TypeSertProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/shopmodel/TypeSertProvider.java
@@ -1,7 +1,7 @@
 package ru.perm.v.el59.office.dao.impl.shopmodel;
 
-import ru.el59.office.shopmodel.TypeSert;
 import ru.perm.v.el59.office.iproviders.shopmodel.ITypeSertProvider;
+import ru.perm.v.el59.office.shopmodel.TypeSert;
 import ru.perm.v.el59.office.wscommand.impl.GenericDaoMessageImpl;
 
 public class TypeSertProvider extends GenericDaoMessageImpl<TypeSert, Long>
