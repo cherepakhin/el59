7a8112d7cf2cd3fc6eefd1ddd9f0ee935ee14ad9
Черепахин Василий <vasi.che@gmail.com>
2024-02-29 16:40:06 +0500

    fix ExpenseProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/shopmodel/ExpenseProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/shopmodel/ExpenseProvider.java
@@ -2,40 +2,40 @@ package ru.perm.v.el59.office.dao.impl.shopmodel;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
-import ru.el59.office.shopmodel.Expense;
-import ru.perm.v.el59.dto.dao.CommonCritery;
+import ru.perm.v.el59.office.iproviders.dao.CommonCritery;
 import ru.perm.v.el59.office.iproviders.shopmodel.IExpenseProvider;
+import ru.perm.v.el59.office.shopmodel.Expense;
 import ru.perm.v.el59.office.wscommand.impl.GenericDaoMessageImpl;
 
 import java.util.List;
 
 
 public class ExpenseProvider extends GenericDaoMessageImpl<Expense, Long>
-		implements IExpenseProvider {
+        implements IExpenseProvider {
 
-	public ExpenseProvider(Class<Expense> type) {
-		super(type);
-	}
+    public ExpenseProvider(Class<Expense> type) {
+        super(type);
+    }
 
-	@Override
-	public List<Expense> getAll() {
-		List<Expense> ret = getByCritery(new CommonCritery(""));
-		return ret;
-	}
+    @Override
+    public List<Expense> getAll() {
+        List<Expense> ret = getByCritery(new CommonCritery(""));
+        return ret;
+    }
 
-	@Override
-	public List<Expense> getNegative() {
-		Criteria criteria = getSession().createCriteria(Expense.class);
-		criteria.add(Restrictions.lt("znak", 0));
-		List<Expense> ret = criteria.list();
-		return ret;
-	}
+    @Override
+    public List<Expense> getNegative() {
+        Criteria criteria = getSession().createCriteria(Expense.class);
+        criteria.add(Restrictions.lt("znak", 0));
+        List<Expense> ret = criteria.list();
+        return ret;
+    }
 
-	@Override
-	public List<Expense> getPositive() {
-		Criteria criteria = getSession().createCriteria(Expense.class);
-		criteria.add(Restrictions.gt("znak", 0));
-		List<Expense> ret = criteria.list();
-		return ret;
-	}
+    @Override
+    public List<Expense> getPositive() {
+        Criteria criteria = getSession().createCriteria(Expense.class);
+        criteria.add(Restrictions.gt("znak", 0));
+        List<Expense> ret = criteria.list();
+        return ret;
+    }
 }
