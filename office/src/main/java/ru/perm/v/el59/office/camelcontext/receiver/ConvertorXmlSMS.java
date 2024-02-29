0fd4018b8482c841de789ce4fa94c5bff4619209
Черепахин Василий <vasi.che@gmail.com>
2024-02-29 18:19:46 +0500

    fix ConvertorXmlSMS

--- office/src/main/java/ru/perm/v/el59/office/camelcontext/receiver/ConvertorXmlSMS.java
+++ office/src/main/java/ru/perm/v/el59/office/camelcontext/receiver/ConvertorXmlSMS.java
@@ -1,14 +1,11 @@
 package ru.perm.v.el59.office.camelcontext.receiver;
 
 import com.thoughtworks.xstream.XStream;
-import org.apache.log4j.Logger;
-import ru.perm.v.el59.office.dto.SMSDTO;
-import ru.perm.v.el59.office.dto.message.MessageSMSDTO;
+import ru.perm.v.el59.dto.SMSDTO;
+import ru.perm.v.el59.dto.message.MessageSMSDTO;
 import ru.perm.v.el59.office.iproviders.shopmodel.ISMSProvider;
 import ru.perm.v.el59.office.shopmodel.SMS;
 import ru.perm.v.el59.office.util.ISMSSender;
-
-import java.util.Date;
 
 public class ConvertorXmlSMS extends ConvertorFromXML<SMSDTO, SMS> {
 
@@ -23,43 +20,43 @@ public class ConvertorXmlSMS extends ConvertorFromXML<SMSDTO, SMS> {
 		xstream.alias(getEntity(), type);
 		try {
 			message = getMessageFromXml(xml);
-			SMSDTO dto = message.getEntity();
-			Logger.getLogger(this.getClass().getName()).info(dto.toString());
-			SMS sms = fillFromDTO(dto, message.getShopCod());
-			if (message.getShopCod() == null) {
-				Logger.getLogger(this.getClass().getName()).error(
-						"ShopCod in message is null");
-			}
-			if (message.getTypeCommand() == null) {
-				Logger.getLogger(this.getClass().getName()).error(
-						"TypeCommand in message is null");
-			}
-			if (message.getTypeCommand() == null) {
-				Logger.getLogger(this.getClass().getName()).error(
-						"TypeCommand in message is null");
-			}
-			if(sms.getPhone()==null || sms.getPhone().isEmpty()) {
-				Logger.getLogger(this.getClass().getName()).error(
-						"Phone is empty in SMS");
-			}
-			if(sms.getMessage()==null || sms.getMessage().isEmpty()) {
-				Logger.getLogger(this.getClass().getName()).error(
-						"Message is empty in SMS");
-			}
-			Logger.getLogger(this.getClass().getName())
-					.info(String
-							.format("Shop: %s;Command %s;Nn: %s,phone: %s, message: %s  ",
-									message.getShopCod(),
-									message.getTypeCommand(), sms.getNn(),
-									sms.getPhone(), sms.getMessage()));
-			doMessage(sms);
-			String ret = getSmsSender().send(sms.getMessage(), sms.getPhone(),sms.getShop());
-			if(ret.isEmpty()) {
-				sms.setDateSend(new Date());
-			} else {
-				sms.setErr(ret);
-			}
-			getDao().update(sms);
+//			SMSDTO dto = message.getEntity();
+//			Logger.getLogger(this.getClass().getName()).info(dto.toString());
+//			SMS sms = fillFromDTO(dto, message.getShopCod());
+//			if (message.getShopCod() == null) {
+//				Logger.getLogger(this.getClass().getName()).error(
+//						"ShopCod in message is null");
+//			}
+//			if (message.getTypeCommand() == null) {
+//				Logger.getLogger(this.getClass().getName()).error(
+//						"TypeCommand in message is null");
+//			}
+//			if (message.getTypeCommand() == null) {
+//				Logger.getLogger(this.getClass().getName()).error(
+//						"TypeCommand in message is null");
+//			}
+//			if(sms.getPhone()==null || sms.getPhone().isEmpty()) {
+//				Logger.getLogger(this.getClass().getName()).error(
+//						"Phone is empty in SMS");
+//			}
+//			if(sms.getMessage()==null || sms.getMessage().isEmpty()) {
+//				Logger.getLogger(this.getClass().getName()).error(
+//						"Message is empty in SMS");
+//			}
+//			Logger.getLogger(this.getClass().getName())
+//					.info(String
+//							.format("Shop: %s;Command %s;Nn: %s,phone: %s, message: %s  ",
+//									message.getShopCod(),
+//									message.getTypeCommand(), sms.getNn(),
+//									sms.getPhone(), sms.getMessage()));
+//			doMessage(sms);
+//			String ret = getSmsSender().send(sms.getMessage(), sms.getPhone(),sms.getShop());
+//			if(ret.isEmpty()) {
+//				sms.setDateSend(new Date());
+//			} else {
+//				sms.setErr(ret);
+//			}
+//			getDao().update(sms);
 			return xml;
 		} catch (Exception e) {
 			e.printStackTrace();
