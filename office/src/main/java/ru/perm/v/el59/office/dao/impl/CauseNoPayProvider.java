60ca6a4d31fe5b1e5244d4c0e9ac17684272ec66
Черепахин Василий <vasi.che@gmail.com>
2024-02-29 17:36:03 +0500

    fix CauseNoPayProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/CauseNoPayProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/CauseNoPayProvider.java
@@ -1,6 +1,6 @@
 package ru.perm.v.el59.office.dao.impl;
 
-import ru.el59.office.db.CauseNoPay;
+import ru.perm.v.el59.office.db.CauseNoPay;
 import ru.perm.v.el59.office.iproviders.ICauseNoPayProvider;
 import ru.perm.v.el59.office.iproviders.dao.CommonCritery;
 
