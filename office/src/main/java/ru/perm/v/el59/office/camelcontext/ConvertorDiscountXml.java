86576a3aa4cecf35844087e9f1008017ac39aa64
Черепахин Василий <vasi.che@gmail.com>
2024-02-29 18:39:37 +0500

    fix ConvertorDiscountXml

--- office/src/main/java/ru/perm/v/el59/office/camelcontext/ConvertorDiscountXml.java
+++ office/src/main/java/ru/perm/v/el59/office/camelcontext/ConvertorDiscountXml.java
@@ -1,8 +1,8 @@
 package ru.perm.v.el59.office.camelcontext;
 
 import com.thoughtworks.xstream.XStream;
-import ru.perm.v.el59.office.dto.DiscountDTO;
-import ru.perm.v.el59.office.dto.message.MessageEntity;
+import ru.perm.v.el59.dto.DiscountDTO;
+import ru.perm.v.el59.dto.message.MessageEntity;
 import ru.perm.v.el59.office.shopmodel.Discount;
 
 public class ConvertorDiscountXml extends ConvertorXML<Discount, DiscountDTO> {
