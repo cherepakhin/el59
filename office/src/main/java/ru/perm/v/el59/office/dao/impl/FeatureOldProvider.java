9591eec37c1694813a67cdb0961f79b9a00938fa
Черепахин Василий <vasi.che@gmail.com>
2024-02-29 17:25:24 +0500

    fix FeatureOldProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/FeatureOldProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/FeatureOldProvider.java
@@ -1,7 +1,7 @@
 package ru.perm.v.el59.office.dao.impl;
 
 
-import ru.el59.office.db.FeatureOld;
+import ru.perm.v.el59.office.db.FeatureOld;
 import ru.perm.v.el59.office.iproviders.IFeatureOldProvider;
 
 public class FeatureOldProvider extends
