e66bbdd38b1fcef260fce114c6d8a00989734a1c
Черепахин Василий <vasi.che@gmail.com>
2024-02-29 16:38:34 +0500

    fix PayPodCardProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/shopmodel/PayPodCardProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/shopmodel/PayPodCardProvider.java
@@ -1,8 +1,7 @@
 package ru.perm.v.el59.office.dao.impl.shopmodel;
 
-
-import ru.el59.office.shopmodel.PayPodCard;
 import ru.perm.v.el59.office.iproviders.shopmodel.IPayPodCardProvider;
+import ru.perm.v.el59.office.shopmodel.PayPodCard;
 
 public class PayPodCardProvider extends APaymentProvider<PayPodCard, Long>
 		implements IPayPodCardProvider {
