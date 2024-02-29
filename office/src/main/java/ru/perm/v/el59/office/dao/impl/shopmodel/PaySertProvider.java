acf8b9f3689f893e644a1dc9ead6668922aa7339
Черепахин Василий <vasi.che@gmail.com>
2024-02-29 16:33:23 +0500

    fix PaySertProvider

--- office/src/main/java/ru/perm/v/el59/office/dao/impl/shopmodel/PaySertProvider.java
+++ office/src/main/java/ru/perm/v/el59/office/dao/impl/shopmodel/PaySertProvider.java
@@ -1,8 +1,8 @@
 package ru.perm.v.el59.office.dao.impl.shopmodel;
 
 
-import ru.el59.office.shopmodel.PaySert;
 import ru.perm.v.el59.office.iproviders.shopmodel.IPaySertProvider;
+import ru.perm.v.el59.office.shopmodel.PaySert;
 
 public class PaySertProvider extends APaymentProvider<PaySert, Long> implements
 		IPaySertProvider {
